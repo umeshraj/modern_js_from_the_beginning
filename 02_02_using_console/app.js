30942976b9c8098d9f7ecc00648e5522911d80bf
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-29 07:53:21 -0500

    Various console.logs

--- 02_02_using_console/app.js
+++ 02_02_using_console/app.js
@@ -1,1 +1,50 @@
-alert("Hello, World!");
+// Single ling comments
+
+/*
+    Multi line comments
+    are sometimes useful
+*/
+
+// log string
+console.log("Hello, World!");
+
+// log numbers
+console.log(123);
+
+// log boolean
+console.log(true);
+
+// log variable
+var greeting = 'Greetings!';
+console.log(greeting);
+
+// log arrays
+console.log([1, 2, 3, 4]);
+
+// log object literal
+console.log({a: 1, b: 2});
+
+// log object literal as a table
+console.table({a: 1, b: 2});
+
+// log errors
+console.error('This is an error');
+
+
+// clear console
+console.clear();
+
+// log warnings
+console.warn('This is a warning');
+
+// time checkers    
+console.time('helloTimer');
+    console.log("Hello, World!");
+    console.log("Hello, World!");
+    console.log("Hello, World!");
+    console.log("Hello, World!");
+    console.log("Hello, World!");
+    console.log("Hello, World!");
+    console.log("Hello, World!");
+    console.log("Hello, World!");
+console.timeEnd('helloTimer');
