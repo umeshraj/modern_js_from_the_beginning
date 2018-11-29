bc3eb0207bc52f5c54c90d2d6d373b3935dc69be
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-29 08:40:45 -0500

    Const usage

--- 02_03_var_let_const/app.js
+++ 02_03_var_let_const/app.js
@@ -23,9 +23,33 @@
 // var firstname = 'Umesh'; // avoid
 
 
-// LET usage
-let name = 'John Doe';
-console.log(name);
+// // LET usage
+// let name = 'John Doe';
+// console.log(name);
 
-name = 'Steve Smith';
-console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// CONST usage
+// const name = 'Umesh';
+// console.log(name);
+// name = 'Raj';  // will not work
+
+// const test;  // will not work
+
+// const object can be modified
+const person = {
+    name: 'Umesh',
+    age: 101
+};
+
+console.log(person);
+
+person.name = 'Sara';
+console.log(person);
+
+// const array can be modified
+const array = [1, 2, 3,]
+console.log(array);
+array.push(10);
+console.log(array);
