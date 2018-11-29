14628cda04cb52bbf7c9849c2830a87bf356fd9b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-29 09:14:09 -0500

    typeof primitives

---
+++ 02_04_datatypes/app.js
@@ -0,0 +1,25 @@
+// PRIMITIVE
+
+// String
+const name = 'Umesh';
+console.log(typeof name);
+
+// Numbers
+const age = 100;
+console.log(typeof age);
+
+// Boolean
+const hasKids = true
+console.log(typeof hasKids);
+
+// Null
+const car = null;
+console.log(typeof car);
+
+// undefined (note use of let instead of const)
+let test;
+console.log(typeof test);
+
+// Symbol
+const sym = Symbol();
+console.log(sym);
