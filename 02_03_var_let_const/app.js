5a2bfc7e035fb3d0b9f72136e03cfad6961fe6b0
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-29 08:31:26 -0500

    Let works like var

--- 02_03_var_let_const/app.js
+++ 02_03_var_let_const/app.js
@@ -1,23 +1,31 @@
 // var, let, const
 
-var name = 'John Doe';
+// // VAR Usage
+// var name = 'John Doe';
+// console.log(name);
+
+// name = 'Steve Smith';
+// console.log(name);
+
+// // init variable
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Cannot start with a number
+
+// // multi-word vars
+// var firstName = 'Umesh';  // camel case
+// var first_name = 'Umesh'; // snake case
+// var FirstName = 'Umesh';  // Pascal case
+// var firstname = 'Umesh'; // avoid
+
+
+// LET usage
+let name = 'John Doe';
 console.log(name);
 
 name = 'Steve Smith';
 console.log(name);
-
-// init variable
-var greeting;
-console.log(greeting);
-greeting = 'Hello';
-console.log(greeting);
-
-// letters, numbers, _, $
-// Cannot start with a number
-
-// multi-word vars
-var firstName = 'Umesh';  // camel case
-var first_name = 'Umesh'; // snake case
-var FirstName = 'Umesh';  // Pascal case
-var firstname = 'Umesh'; // avoid
-
