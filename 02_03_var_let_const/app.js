7f3ca9c231a86bec4dc583fc0eacee7c471c792d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-29 08:29:43 -0500

    Using var

---
+++ 02_03_var_let_const/app.js
@@ -0,0 +1,23 @@
+// var, let, const
+
+var name = 'John Doe';
+console.log(name);
+
+name = 'Steve Smith';
+console.log(name);
+
+// init variable
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+// letters, numbers, _, $
+// Cannot start with a number
+
+// multi-word vars
+var firstName = 'Umesh';  // camel case
+var first_name = 'Umesh'; // snake case
+var FirstName = 'Umesh';  // Pascal case
+var firstname = 'Umesh'; // avoid
+
