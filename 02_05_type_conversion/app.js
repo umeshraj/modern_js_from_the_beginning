9241aa1511a89fd3c6aa1913d46d4506a08da1cb
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-30 07:18:39 -0500

    Boolean to string

--- 02_05_type_conversion/app.js
+++ 02_05_type_conversion/app.js
@@ -3,6 +3,11 @@ let val;
 // Number to string
 val = String(512);
 val = String(10 + 12);
+
+// Boolean to string
+val = String(true);
+
+
 console.log(val);
 console.log(typeof val);
 console.log(val.length);
