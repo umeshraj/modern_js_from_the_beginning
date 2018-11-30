38700c8096987f519f4676338320e39a073f286b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-30 07:50:48 -0500

    Various type conversions

--- 02_05_type_conversion/app.js
+++ 02_05_type_conversion/app.js
@@ -20,3 +20,29 @@ val = (true).toString();
 console.log(val);
 console.log(typeof val);
 console.log(val.length);
+
+// String to number
+console.log('To numbers');
+
+val = Number('512');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1, 2, 3]);
+
+// Other ways to parse
+val = parseInt('100.1');
+val = parseFloat('10.12');
+
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+// JS type coercion
+const val1 = String(5);
+const val2 = 6;
+const valSum = val1 + val2;
+
+console.log(valSum);
+console.log(typeof valSum);
