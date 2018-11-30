07c54be358d820360a9cffb141d26e9ba9fa2260
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-11-30 07:25:03 -0500

    toString method

--- 02_05_type_conversion/app.js
+++ 02_05_type_conversion/app.js
@@ -13,6 +13,10 @@ val = String(new Date());
 // Array to string
 val = String([1, 2, 3])
 
+// toString() method
+val = (512).toString();
+val = (true).toString();
+
 console.log(val);
 console.log(typeof val);
 console.log(val.length);
