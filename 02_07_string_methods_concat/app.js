42175ec23c0575ad756ed9a7328f562fbabe97bb
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-12-01 08:21:40 -0500

    Various string methods

--- 02_07_string_methods_concat/app.js
+++ 02_07_string_methods_concat/app.js
@@ -1,28 +1,61 @@
-const num1 = 100;
-const num2 = 40;
+const firstName = 'Umesh';
+const lastName = 'Rajashekar';
+const age = 97;
+const str = 'Hello. My name is Umesh. I am a boy.'
+const tags = 'math,science,ml,dl';
+
 let val;
 
-// Simple math
-val = num1 + num2;
-val = num1 - num2;
-val = num1 * num2;
-val = num1 / num2;
-val = num1 % num2;
+// string concatenation
+val = firstName + lastName;
+val = firstName + ' ' + lastName;
 
-// Math object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.8);
-val = Math.ceil(2.4);
-val = Math.floor(2.9);
-val = Math.sqrt(144);
-val = Math.abs(-2);
-val = Math.pow(2, 4);
-val = Math.min(2, 3, 4, 5, -1);
-val = Math.max(2, 3, 4, 5, -1);
-val = Math.random();
+// Append
+val = 'umesh';
+val += ' rajashekar';
 
-// int random number
-val = Math.floor(Math.random() * 20 + 1);
+val = "Hello! My name is " + firstName + " and I am " + age;
+
+// Escaping
+val = 'That\'s awesome. I can\'t wait.';
+
+// Length
+val = firstName.length;
+
+// concat method
+val = firstName.concat(' ', lastName, 'test');
+
+// change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+// Extract index
+val = firstName[0];
+
+// indexOf
+val = firstName.indexOf('m');
+val = firstName.lastIndexOf('s');
+
+// charAt
+val = firstName.charAt(2)
+// get last character
+val = firstName.charAt(firstName.length-1);
+
+// substring
+val = firstName.substring(1, 3);
+
+// slice (similar to substring)
+val = firstName.slice(1, 3);
+val = firstName.slice(-3);
+
+// split()
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Umesh', 'Moose');
+
+// includes()
+val = str.includes('Hello');
 
 console.log(val);
