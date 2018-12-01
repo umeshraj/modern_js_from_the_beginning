28fd0d70943a583cdc52d57ec7b826506d245652
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-12-01 07:32:58 -0500

    Base code for 2.7, lecture 10

---
+++ 02_07_string_methods_concat/app.js
@@ -0,0 +1,28 @@
+const num1 = 100;
+const num2 = 40;
+let val;
+
+// Simple math
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.ceil(2.4);
+val = Math.floor(2.9);
+val = Math.sqrt(144);
+val = Math.abs(-2);
+val = Math.pow(2, 4);
+val = Math.min(2, 3, 4, 5, -1);
+val = Math.max(2, 3, 4, 5, -1);
+val = Math.random();
+
+// int random number
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
