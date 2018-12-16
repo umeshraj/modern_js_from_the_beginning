5ae742cebdec06b4cd8753f04af9fbd5550c845c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-12-16 13:35:38 -0500

    Creating page from scratch!

---
+++ 04_01_task_list_part_01/app.js
@@ -0,0 +1,44 @@
+// Define UI vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listerners
+loadEventListeners();
+
+// load all event listeners
+function loadEventListeners(){
+    // Add task event
+    form.addEventListener('submit', addTask);
+}
+
+// Add task
+function addTask(e){
+    if (taskInput.value === ''){
+        alert('Add a task');
+    }else{
+        // create li element
+        const li = document.createElement('li');
+        // add className to help materialize
+        li.className = 'collection-item';
+        // create text node and append to li
+        li.appendChild(document.createTextNode(taskInput.value));
+        // create new link element
+        const link = document.createElement('a');
+        // Add class
+        link.className = 'delete-item secondary-content';
+        // Add icon html
+        link.innerHTML = '<i class = "fa fa-remove" </i>';
+        // Append link to li
+        li.appendChild(link);
+
+        // Append li to ul
+        taskList.appendChild(li);
+
+        // Clear the input
+        taskInput.value = '';
+    }
+    e.preventDefault();
+}
