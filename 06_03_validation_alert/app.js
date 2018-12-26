0abf389a76c916bb1f34f480b73c7ce00b518e10
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-12-26 07:08:55 -0500

    Base code for 6.3, lecture 52

---
+++ 06_03_validation_alert/app.js
@@ -0,0 +1,51 @@
+// Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI constructor
+function UI() {}
+
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById("book-list");
+  // create tr element
+  const row = document.createElement("tr");
+  // insert cols
+  row.innerHTML = `
+        <td> ${book.title} </td>
+        <td> ${book.author} </td>
+        <td> ${book.isbn} </td>
+        <td> <a href="#" class="delete"> X </a> </td>
+        `;
+  list.appendChild(row);
+};
+
+UI.prototype.clearFields = function() {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
+// Event listeners
+document.getElementById("book-form").addEventListener("submit", function(e) {
+  // Get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  // Instantiate book object
+  const book = new Book(title, author, isbn);
+
+  // Instantiate ui object
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear fields
+  ui.clearFields();
+
+  e.preventDefault();
+});
