237b610a83b5eace1508f9d415644a010ac2f498
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-12-31 07:17:26 -0500

    Fix text file name and output data to div

--- 07_11_fetch_api/app.js
+++ 07_11_fetch_api/app.js
@@ -1,12 +1,13 @@
 document.getElementById("button1").addEventListener("click", getText);
 
 function getText() {
-  fetch("test1.txt")
+  fetch("test.txt")
     .then(function(res) {
       return res.text();
     })
     .then(function(data) {
       console.log(data);
+      document.getElementById("output").innerHTML = data;
     })
     .catch(function(err) {
       console.log(err);
