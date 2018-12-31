7eaa752ffbf2a4b1ad4b44142df34680fda13f2b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-12-31 07:28:41 -0500

    Getting json data from external api (github)

--- 07_11_fetch_api/app.js
+++ 07_11_fetch_api/app.js
@@ -1,5 +1,6 @@
 document.getElementById("button1").addEventListener("click", getText);
 document.getElementById("button2").addEventListener("click", getJSON);
+document.getElementById("button3").addEventListener("click", getExternal);
 
 // Get local text data
 function getText() {
@@ -28,6 +29,26 @@ function getJSON() {
       let output = "";
       data.forEach(function(post) {
         output += `<li>${post.title}</li>`;
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get external JSON
+function getExternal() {
+  fetch("https://api.github.com/users")
+    .then(function(res) {
+      // console.log(res);
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = "";
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;
       });
       document.getElementById("output").innerHTML = output;
     })
