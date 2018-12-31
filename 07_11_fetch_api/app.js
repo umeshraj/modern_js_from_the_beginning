9c36aabe14166ede31f3bd7cf8ed1de221af59c4
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2018-12-31 07:24:42 -0500

    Using fetch to get local json data

--- 07_11_fetch_api/app.js
+++ 07_11_fetch_api/app.js
@@ -1,5 +1,7 @@
 document.getElementById("button1").addEventListener("click", getText);
+document.getElementById("button2").addEventListener("click", getJSON);
 
+// Get local text data
 function getText() {
   fetch("test.txt")
     .then(function(res) {
@@ -8,6 +10,26 @@ function getText() {
     .then(function(data) {
       console.log(data);
       document.getElementById("output").innerHTML = data;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get local JSON data
+function getJSON() {
+  fetch("post.json")
+    .then(function(res) {
+      // console.log(res);
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = "";
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
+      });
+      document.getElementById("output").innerHTML = output;
     })
     .catch(function(err) {
       console.log(err);
