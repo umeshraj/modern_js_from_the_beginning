7f72d2156e49ab6fb0759ebf1606dfa4776ce515
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-01 07:54:40 -0500

    A cleaner example of async await with fetch

--- 07_14_async_await/app.js
+++ 07_14_async_await/app.js
@@ -1,20 +1,33 @@
-async function myFunc() {
-  // return "Hello";
+// async function myFunc() {
+//   // return "Hello";
 
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("Hello"), 1000);
-  });
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("Hello"), 1000);
+//   });
 
-  const error = false;
-  if (!error) {
-    const res = await promise; // wait until promise is resolved
-    return res;
-  } else {
-    await Promise.reject(new Error("Something went wrong"));
-  }
+//   const error = false;
+//   if (!error) {
+//     const res = await promise; // wait until promise is resolved
+//     return res;
+//   } else {
+//     await Promise.reject(new Error("Something went wrong"));
+//   }
+// }
+
+// // console.log(myFunc());
+// myFunc()
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err));
+
+async function getUsers() {
+  // await the response of the fetch call
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  //   only proceed once the promise is resolved
+  const data = await response.json();
+
+  //   Only proceed once second promise is resolved
+  return data;
 }
 
-// console.log(myFunc());
-myFunc()
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
+getUsers().then(users => console.log(users));
