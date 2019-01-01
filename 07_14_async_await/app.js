a0c4585294f35422289e4e2432bd797412310363
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-01 07:45:14 -0500

    Using await and catch

--- 07_14_async_await/app.js
+++ 07_14_async_await/app.js
@@ -5,9 +5,16 @@ async function myFunc() {
     setTimeout(() => resolve("Hello"), 1000);
   });
 
-  const res = await promise; // wait until promise is resolved
-  return res;
+  const error = false;
+  if (!error) {
+    const res = await promise; // wait until promise is resolved
+    return res;
+  } else {
+    await Promise.reject(new Error("Something went wrong"));
+  }
 }
 
 // console.log(myFunc());
-myFunc().then(res => console.log(res));
+myFunc()
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
