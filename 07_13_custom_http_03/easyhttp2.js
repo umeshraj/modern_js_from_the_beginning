699ae4046ac325b0b76f516a4189c359c282cb94
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-01 06:57:57 -0500

    Returning data to main via promise

--- 07_13_custom_http_03/easyhttp2.js
+++ 07_13_custom_http_03/easyhttp2.js
@@ -11,9 +11,11 @@
 class EasyHTTP {
   //  make and HTTP GET request
   get(url) {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
   }
 }
