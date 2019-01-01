becad727efa917176544943c77f7e468fedd6cd1
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-01 07:04:07 -0500

    POST request

--- 07_13_custom_http_03/app.js
+++ 07_13_custom_http_03/app.js
@@ -1,7 +1,21 @@
 const http = new EasyHTTP();
 
-// Get Users
+// // Get Users
+// http
+//   .get("https://jsonplaceholder.typicode.com/users")
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Posting data
+
+// user data
+const data = {
+  name: "John Doe",
+  username: "johndoe",
+  email: "jdoe@gmail.com"
+};
+// create post
 http
-  .get("https://jsonplaceholder.typicode.com/users")
+  .post("https://jsonplaceholder.typicode.com/users", data)
   .then(data => console.log(data))
   .catch(err => console.log(err));
--- 07_13_custom_http_03/easyhttp2.js
+++ 07_13_custom_http_03/easyhttp2.js
@@ -9,10 +9,26 @@
  */
 
 class EasyHTTP {
-  //  make and HTTP GET request
+  //  make HTTP GET request
   get(url) {
     return new Promise((resolve, reject) => {
       fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  //  make HTTP POST request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
         .then(res => res.json())
         .then(data => resolve(data))
         .catch(err => reject(err));
