67a8f4fdc5a59fb1b6b891ce07b4b7d86c3b34ba
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-01 09:27:32 -0500

    Base code for 7.15, lecture 70

---
+++ 07_15_custom_http_04_async_await/app.js
@@ -0,0 +1,33 @@
+const http = new EasyHTTP();
+
+// // Get Users
+// http
+//   .get("https://jsonplaceholder.typicode.com/users")
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// user data
+const data = {
+  name: "John Doe",
+  username: "johndoe",
+  email: "jdoe@gmail.com"
+};
+
+// // Posting data
+// // create user
+// http
+//   .post("https://jsonplaceholder.typicode.com/users", data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// // Update user with PUT
+// http
+//   .put("https://jsonplaceholder.typicode.com/users/2", data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Remove user with DELETE
+http
+  .delete("https://jsonplaceholder.typicode.com/users/2")
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
