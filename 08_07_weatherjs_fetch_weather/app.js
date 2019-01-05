e9f159c08820c949cfbc8f22443ec0619a060a17
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-05 09:36:20 -0500

    Calling weather only after DOM has loaded

--- 08_07_weatherjs_fetch_weather/app.js
+++ 08_07_weatherjs_fetch_weather/app.js
@@ -1,7 +1,12 @@
 // Init weather object
 const weather = new Weather("Boston", "US");
 
-weather
-  .getWeather()
-  .then(results => console.log(results))
-  .catch(err => console.log(err));
+// Get weather on DOM loaded
+document.addEventListener("DOMContentLoaded", getWeather);
+
+function getWeather() {
+  weather
+    .getWeather()
+    .then(results => console.log(results))
+    .catch(err => console.log(err));
+}
