f4255190d894d701b2df5e90d50390b97ebd21ba
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-05 09:48:32 -0500

    Moving async getApiKey to method for security. Ugly reload each time

--- 08_07_weatherjs_fetch_weather/weather.js
+++ 08_07_weatherjs_fetch_weather/weather.js
@@ -1,8 +1,6 @@
 class Weather {
   constructor(city, countryCode) {
     this.apiKey = null;
-    // update api key from local file
-    this.getApiKey();
     this.city = city;
     this.countryCode = countryCode;
   }
@@ -16,12 +14,15 @@ class Weather {
 
   //   fetch weather from api
   async getWeather() {
+    //   Get apikey from local file
+    const apiKey = await this.getApiKey();
+    // create url
     const url = `
         http://api.openweathermap.org/data/2.5/weather?q=${this.city},${
       this.countryCode
     }&APPID=${this.apiKey}
       `;
-    console.log(url);
+    //   Get data
     const response = await fetch(url);
     const responseData = await response.json();
     return responseData.main;
