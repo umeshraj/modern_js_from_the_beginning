fc22b086b7709e8d79ef94eb7719829afe433061
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-17 08:30:28 -0500

    commonjs and es2015 style imports

--- 13_02_intro_es2015_modules/src/app.js
+++ 13_02_intro_es2015_modules/src/app.js
@@ -1,10 +1,16 @@
-const greeting = "Hello World";
+// import with commonjs syntax
+// const person = require("./mymodule1");
+
+// import ES2015 module style
+// import { person, sayHello } from "./mymodule2";
+// console.log(person.name);
+// console.log(sayHello());
+
+// import all things
+// import * as mod from "./mymodule2";
+// console.log(mod.person.name);
+// console.log(mod.sayHello());
+
+// default importing
+import greeting from "./mymodule2";
 console.log(greeting);
-
-// const getData = async (url) => {
-//   const response = await fetch(url);
-//   const result = await response.json();
-//   console.log(result);
-// };
-
-// getData('https://jsonplaceholder.typicode.com/posts');
---
+++ 13_02_intro_es2015_modules/src/mymodule2.js
@@ -0,0 +1,12 @@
+export const person = {
+  name: "Umesh",
+  age: 30
+};
+
+export function sayHello() {
+  return `Hello ${person.name}`;
+}
+
+// defaults
+const greeting = "Hello World";
+export default greeting;
