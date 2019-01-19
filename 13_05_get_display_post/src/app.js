72d48a63be8088f4f60bc7ce83434d0e6ffb5e6b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2019-01-19 07:16:31 -0500

    Import the easyhttp library and load posts from our json server

---
+++ 13_05_get_display_post/src/app.js
@@ -0,0 +1,11 @@
+import { http } from "./http";
+
+// get posts on dom load
+document.addEventListener("DOMContentLoaded", getPosts);
+
+function getPosts() {
+  http
+    .get("http://localhost:3000/posts")
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
